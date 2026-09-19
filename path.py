2627c93930e9eeb5b0a0c5150d4900f7b901bf6e
Akash Chaudhary <chaudhary.akash@proton.me>
2026-09-19 10:36:34 +0530

    Updated path script

--- path.py
+++ path.py
@@ -1,7 +1,6 @@
 import os
 
 out = "path.txt"
-for i in range(len(os.listdir("mooc-java-programming-i"))):
-    print(os.listdir("mooc-java-programming-i")[i])
+for i in range(1, len(os.listdir("mooc-java-programming-i"))):
     with open("path.txt", "a", encoding="utf-8") as f:
         f.write(os.listdir("mooc-java-programming-i")[i] + "\n")
